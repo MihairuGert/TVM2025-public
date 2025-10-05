225a313a679c1ce17a78116dab68b7a5f94a0abf
mikhail <vertygert4949@gmail.com>
2025-10-05 18:23:19 +0700

    lab3

--- lab02/src/calculate.ts
+++ lab02/src/calculate.ts
@@ -4,10 +4,11 @@ export const rpnCalc = {
     number(arg0) {
         return parseInt(arg0.sourceString)
     },
-    Expr_sum(arg0 : any, arg1, arg2 : any) {
-        return arg0.calculate() + arg1.calculate()
-    },
-    Expr_mul(arg0 : any, arg1, arg2 : any) {
-        return arg0.calculate() * arg1.calculate()
+    Expr_bin(arg0 : any, arg1, arg2 : any) {
+        if (arg2.sourceString == "+")
+            return arg0.calculate() + arg1.calculate()
+        if (arg2.sourceString == "*")
+            return arg0.calculate() * arg1.calculate()
+        throw 1
     },
 } satisfies ReversePolishNotationActionDict<number>;
--- lab02/src/stackDepth.ts
+++ lab02/src/stackDepth.ts
@@ -4,14 +4,7 @@ export const rpnStackDepth = {
     number(arg0) {
         return { max: 1, out: 1 };
     },  
-    Expr_sum(arg0 : any, arg1 : any, arg2) {
-        const stack1 = arg0.stackDepth;
-        const stack2 = arg1.stackDepth;
-        const res = stack1.out + stack2.out;
-        const mx = Math.max(stack1.max, stack2.max + stack1.out);
-        return {max: mx, out: res - 1};
-    },
-    Expr_mul(arg0 : any, arg1 : any, arg2) {
+    Expr_bin(arg0 : any, arg1 : any, arg2) {
         const stack1 = arg0.stackDepth;
         const stack2 = arg1.stackDepth;
         const res = stack1.out + stack2.out;
